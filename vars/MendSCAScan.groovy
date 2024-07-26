6bb6830153e3c17b2fc635e3e4697dbec0661c30
mayshelef <148444287+mayshelef@users.noreply.github.com>
2024-07-26 20:21:54 +0300

    Update MendSCAScan.groovy

--- vars/MendSCAScan.groovy
+++ vars/MendSCAScan.groovy
@@ -1,17 +1,18 @@
 def call() { 
-      echo 'Run Mend dependencies scan'
-      catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE'){
-            sh '''
+    echo 'Run Mend dependencies scan'
+    catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE'){
+        sh '''
+            set -x
             export repo=$(basename -s .git $(git config --get remote.origin.url))
             export branch=$(git rev-parse --abbrev-ref HEAD)
             /usr/local/bin/mend dep -u -s "*//${JOB_NAME}//${repo}_${branch}" --fail-policy --non-interactive --export-results dep-results.txt
+            dep_exit=$?
             if [[ "$dep_exit" == "9" ]]; then
                   echo "[warning]  Dependency scan policy violation"
             else
                   echo "No policy violations found in dependencies scan"
             fi          
-            '''
-      }
-      archiveArtifacts artifacts: "dep-results.txt", fingerprint: true
-
+        '''
+    }
+    archiveArtifacts artifacts: "dep-results.txt", fingerprint: true
 }
