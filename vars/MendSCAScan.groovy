0b7d1da678819e33214ef4b458422922048b8a60
mayshelef <148444287+mayshelef@users.noreply.github.com>
2024-07-26 20:22:58 +0300

    Update MendSCAScan.groovy

--- vars/MendSCAScan.groovy
+++ vars/MendSCAScan.groovy
@@ -1,16 +1,19 @@
 def call() { 
     echo 'Run Mend dependencies scan'
-    catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE'){
+    catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE') {
         sh '''
             set -x
+            export MEND_URL=${MEND_URL}
+            export MEND_EMAIL=${MEND_EMAIL}
+            export MEND_USER_KEY=${MEND_USER_KEY}
             export repo=$(basename -s .git $(git config --get remote.origin.url))
             export branch=$(git rev-parse --abbrev-ref HEAD)
             /usr/local/bin/mend dep -u -s "*//${JOB_NAME}//${repo}_${branch}" --fail-policy --non-interactive --export-results dep-results.txt
             dep_exit=$?
             if [[ "$dep_exit" == "9" ]]; then
-                  echo "[warning]  Dependency scan policy violation"
+                echo "[warning]  Dependency scan policy violation"
             else
-                  echo "No policy violations found in dependencies scan"
+                echo "No policy violations found in dependencies scan"
             fi          
         '''
     }
