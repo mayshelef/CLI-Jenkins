bb16a6eec90bfed3bbe49fc66bf0a80b21f86a99
mayshelef <148444287+mayshelef@users.noreply.github.com>
2024-07-28 18:09:20 +0300

    Update DownloadMendCLI.groovy

--- vars/DownloadMendCLI.groovy
+++ vars/DownloadMendCLI.groovy
@@ -1,4 +1,4 @@
 def call() { 
       echo 'Downloading Mend CLI'
-      sh 'curl https://downloads.mend.io/cli/linux_amd64/mend -o /usr/local/bin/mend && chmod +x /usr/local/bin/mend'
+      sh 'curl -LJO https://downloads.mend.io/production/unified/latest/linux_amd64/mend && chmod +x mend'
 }
